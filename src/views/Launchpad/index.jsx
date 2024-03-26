43528b98aff428c56885cbaa8bf198d2869468c2
jackie lee <catiga03@gmail.com>
2024-03-26 18:46:31 +0800

    1

--- src/views/Launchpad/index.jsx
+++ src/views/Launchpad/index.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import { Button, Form, Input, Row, Col, Slider } from "antd";
 import Web3 from 'web3';
